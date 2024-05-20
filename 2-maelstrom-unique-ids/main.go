42f46e220ce905de633240c7eeaeaf8930971680
Akshat Sharma <akshatsharma2602@gmail.com>
2024-05-20 11:12:52 -0400

    added unique id comments

--- 2-maelstrom-unique-ids/main.go
+++ 2-maelstrom-unique-ids/main.go
@@ -19,6 +19,9 @@ var logger = log.New(os.Stderr, "", 0)
 func (s *server) generateUniqueID() int64 {
 	nodeID := []byte(s.n.ID())[0]
 
+	// My original solution was using the node ID and the time as seed but I feel like it might not be that cryptographically secure
+	// Using crypto/rand might be a good option similar to this https://github.com/teivah/gossip-glomers/blob/main/challenge-2-unique-id/main.go
+	// I'm not sure if this is the best way to do it but I think it's a good start
 	random := rand.New(rand.NewSource(time.Now().UnixNano() + int64(nodeID)))
 	r := random.Int63()
 	logger.Printf("Generated ID: %d\n", r)
