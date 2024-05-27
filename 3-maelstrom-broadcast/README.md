dde1a561c4ea6dbacac4db2bf6e5a15d83e3445b
Akshat Sharma <akshatsharma2602@gmail.com>
2024-05-27 07:23:42 -0400

    3b done

--- 3-maelstrom-broadcast/README.md
+++ 3-maelstrom-broadcast/README.md
@@ -1,5 +1,9 @@
-1st problem: type conversion due to maelstrom's way of sending body
+1st problem: type conversion due to maelstrom's way of sending body, not really a problem
 
 2nd problem: deciding on whether to do some kind of graph traversal and send that data to the nodes or just do atleast once semantics and ignore retransmission if the node has already seen this particular value before
 
-3rd problem: When and how to clear the channel to ensure no messages are lost
+3rd problem: When and how to clear the channel to ensure no messages are lost. Not really a problem because channels are blocking
+
+4th problem: concurrent map writes, need to add a 
+
+5th problem: performance
--- 3-maelstrom-broadcast/main.go
+++ 3-maelstrom-broadcast/main.go
@@ -5,19 +5,18 @@ import (
 	"errors"
 	"log"
 	"os"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
-
-type broadcastData struct {
-	message []float64
-}
 
 type server struct {
 	node              *maelstrom.Node
 	topology          map[string][]string
 	pendingBroadcasts chan int
-	broadcastData     broadcastData
+	broadcastData     map[float64]struct{}
+	// A mutex to lock the broadcastData map
+	messageLock sync.Mutex
 }
 
 type broadcastMessage struct {
@@ -25,8 +24,11 @@ type broadcastMessage struct {
 	Message int    `json:"message"`
 }
 
+type topologyMessage struct {
+	Topology map[string][]string `json:"topology"`
+}
+
 // Nitpick: Currently using a global logger, should be passed to the server struct
-// TODO: Implement atleast once semantics, right now all values are stored as is and marked for rebroadcast
 
 var logger = log.New(os.Stderr, "", 0)
 
@@ -34,6 +36,10 @@ func (s *server) broadcastValues() {
 	for {
 		nodeID := s.node.ID()
 		neighbors := s.topology[nodeID]
+		if len(neighbors) == 0 {
+			// logger.Printf("No neighbors found for node: %s", nodeID)
+			continue
+		}
 		logger.Printf("Waiting for pending broadcasts")
 		message := <-s.pendingBroadcasts
 
@@ -65,11 +71,7 @@ func (s *server) broadcastValues() {
 				return nil
 			})
 		}
-		// Clear the pending broadcasts
-		// TODO: Check if a lock is needed here
-		s.pendingBroadcasts = make(chan int, 10)
 	}
-
 }
 
 func (s *server) handleRead(msg maelstrom.Message) error {
@@ -78,11 +80,16 @@ func (s *server) handleRead(msg maelstrom.Message) error {
 		return err
 	}
 
-	body["type"] = "read_ok"
 	logger.Printf("Read request received: %v", body)
-	body["messages"] = s.broadcastData.message
-	logger.Printf("Read response: %v", body)
-	return s.node.Reply(msg, body)
+
+	ids := []float64{}
+	s.messageLock.Lock()
+	defer s.messageLock.Unlock()
+	for id := range s.broadcastData {
+		ids = append(ids, id)
+	}
+
+	return s.node.Reply(msg, map[string]any{"type": "read_ok", "messages": ids})
 }
 
 func (s *server) handleBroadcast(msg maelstrom.Message) error {
@@ -92,37 +99,28 @@ func (s *server) handleBroadcast(msg maelstrom.Message) error {
 	}
 
 	logger.Printf("Broadcast request received: %v", body)
-	body["type"] = "broadcast_ok"
-	s.broadcastData.message = append(s.broadcastData.message, body["message"].(float64))
+	s.messageLock.Lock()
+	defer s.messageLock.Unlock()
+	if _, exists := s.broadcastData[body["message"].(float64)]; exists {
+		logger.Printf("Message already exists in local state")
+		return s.node.Reply(msg, map[string]any{"type": "broadcast_ok"})
+	}
+	s.broadcastData[body["message"].(float64)] = struct{}{}
 	s.pendingBroadcasts <- int(body["message"].(float64))
-	logger.Printf("Added message to local state: %v", s.broadcastData.message)
-	// delete the key message from the body
-	delete(body, "message")
-	return s.node.Reply(msg, body)
+
+	logger.Printf("Added message to local state")
+	return s.node.Reply(msg, map[string]any{"type": "broadcast_ok"})
 }
 
 func (s *server) handleTopology(msg maelstrom.Message) error {
-	var body map[string]any
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
+	var t topologyMessage
+	if err := json.Unmarshal(msg.Body, &t); err != nil {
 		return err
 	}
 
-	body["type"] = "topology_ok"
-	// body["topology"] is of type map[string]interface{} but we need to convert it to map[string][]string
-	// to be able to use it in the broadcastValues function
-	topology := map[string][]string{}
-	for k, v := range body["topology"].(map[string]interface{}) {
-		neighbors := []string{}
-		for _, neighbor := range v.([]interface{}) {
-			neighbors = append(neighbors, neighbor.(string))
-		}
-		topology[k] = neighbors
-	}
-	s.topology = topology
+	s.topology = t.Topology
 
-	// delete the key topology from the body
-	delete(body, "topology")
-	return s.node.Reply(msg, body)
+	return s.node.Reply(msg, map[string]any{"type": "topology_ok"})
 }
 
 func main() {
@@ -131,7 +129,7 @@ func main() {
 		node:              n,
 		topology:          map[string][]string{},
 		pendingBroadcasts: make(chan int, 10),
-		broadcastData:     broadcastData{message: []float64{}},
+		broadcastData:     map[float64]struct{}{},
 	}
 
 	n.Handle("broadcast", s.handleBroadcast)
